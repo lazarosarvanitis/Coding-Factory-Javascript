ffd2ad811dda71b881520d2ac44afd754cf04d92
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-02-13 16:08:05 +0200

    first commit

---
+++ hello.js
@@ -0,0 +1,4 @@
+/**
+ * This is a simple JavaScript program that prints "Hello, World!" 
+ */
+console.log("Hello, World!");
---
+++ number/celcius.js
@@ -0,0 +1,8 @@
+let celsius = 25; // Temperature in Celsius
+let fahr = 0;
+
+// Convert Celsius to Fahrenheit
+fahr = (celsius * 9/5) + 32;
+
+// Output the result
+console.log(celsius + "°C is equal to " + fahr + "°F");
