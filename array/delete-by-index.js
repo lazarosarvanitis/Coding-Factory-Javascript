d617824eeaa1ec29194b76c5d93c90cc6cc5a4f3
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-02-13 18:56:39 +0200

    feat: update

--- array/delete-by-index.js
+++ array/delete-by-index.js
@@ -1,5 +1,7 @@
-const arr = [1, 2, 3, 4, 5];
+const arr = [10, 2, 9, 6, 5];
 
-//delete by index
+// delete by index
+let itemToRemove = arr.indexOf(9);
+arr.splice(itemToRemove, 1); // Removes the item at the specified index
 
-let 
+console.log(arr); // Output: [10, 2, 6, 5]
